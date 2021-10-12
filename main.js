4e08a974cde0e4edec7979050be8d6eecc56f0f0
Amrod92 <manlio92--@live.it>
2021-10-12 23:27:08 +0100

    roles and about

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
+// Set environment
 process.env.NODE_ENV = 'development';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -15,9 +17,22 @@ function createMainWindow() {
     icon: './assets/icons/Icon_256x256.png',
     resizable: isDev ? true : false
   });
-
   // Render UI front-end electron
   mainWindow.loadFile('./app/index.html');
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: './assets/icons/Icon_256x256.png',
+    resizable: false,
+    autoHideMenuBar: true
+  });
+
+  // Render UI front-end electron
+  aboutWindow.loadFile('./app/about.html');
 }
 
 // This method will be called when Electron has finished
@@ -26,12 +41,71 @@ function createMainWindow() {
 app.whenReady().then(() => {
   createMainWindow();
 
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('ready', () => (mainWindow = null));
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   });
 });
+
+// Main menu
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          subname: [
+            {
+              label: 'About',
+              click: createAboutWindow
+            }
+          ]
+        }
+      ]
+    : []),
+  {
+    role: 'fileMenu'
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow
+            }
+          ]
+        }
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            {
+              role: 'reload'
+            },
+            {
+              role: 'forcereload'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              role: 'toggledevtools'
+            }
+          ]
+        }
+      ]
+    : [])
+];
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
